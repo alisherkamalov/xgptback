e80ac4f87eb54aaba3343538fbfaf8df842c9e16
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-07 19:06:01 +0500

    Update views.py

--- gptapp/views.py
+++ gptapp/views.py
@@ -1,18 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 import g4f
 
-@method_decorator(csrf_exempt, name='dispatch')
 class AskView(APIView):
     def post(self, request):
         try:
             question = request.data.get('question')
             if not question:
-                return self._create_response({"error": "Question is required"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Question is required"}, status=status.HTTP_400_BAD_REQUEST)
 
             # Запрос к API g4f
             response = g4f.ChatCompletion.create(model=g4f.models.gpt_4o_mini, messages=[{"role": "user", "content": question}])
@@ -21,19 +17,13 @@ class AskView(APIView):
             if isinstance(response, str):
                 gpt_response = response
             else:
-                return self._create_response({"error": "Unexpected response format"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"error": "Unexpected response format"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
             if not gpt_response:
-                return self._create_response({"error": "Failed to get a response from GPT-4"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"error": "Failed to get a response from GPT-4"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-            return self._create_response({"answer": gpt_response}, status=status.HTTP_200_OK)
+            return Response({"answer": gpt_response}, status=status.HTTP_200_OK)
 
         except Exception as e:
             print(f"Error in AskView: {e}")  # Отладка ошибок
-            return self._create_response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def _create_response(self, data, status):
-        response = JsonResponse(data, status=status)
-        response["Access-Control-Allow-Origin"] = "https://g-gpt.vercel.app"
-        return response
-
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
