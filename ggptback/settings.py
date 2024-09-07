0b009797e0fdb26c7c2bea607dd72cc183ddc8bc
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-07 18:22:10 +0500

    Update settings.py

--- ggptback/settings.py
+++ ggptback/settings.py
@@ -87,16 +87,13 @@ WSGI_APPLICATION = 'ggptback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 CORS_ALLOWED_ORIGINS = [
-    "https://g-gpt.vercel.app",
+    '*'
 ]
 
 # Password validation
