f7e6f3c844d1fe0087509c866a7b5a046e64f710
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-08 16:52:12 +0500

    Update views.py

--- gptapp/views.py
+++ gptapp/views.py
@@ -1,29 +1,42 @@
+import g4f
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-import g4f
 
 class AskView(APIView):
+    
+    context = []
+
     def post(self, request):
         try:
             question = request.data.get('question')
             if not question:
                 return Response({"error": "Question is required"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            
+            self.context.append({"role": "user", "content": question})
 
-            # Запрос к API g4f
-            response = g4f.ChatCompletion.create(model='gpt-4-turbo', provider=g4f.Provider.Nexra, messages=[{"role": "user", "content": question}])
-
-            # Проверка и обработка ответа
+            
+            response = g4f.ChatCompletion.create(
+                model=g4f.models.gpt_4_turbo,
+                provider=g4f.Provider.Nexra,
+                messages=self.context
+            )
+            
+            
             if isinstance(response, str):
                 gpt_response = response
             else:
                 return Response({"error": "Unexpected response format"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
             if not gpt_response:
-                return Response({"error": "Failed to get a response from gpt"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"error": "Failed to get a response from GPT-4"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            
+            self.context.append({"role": "assistant", "content": gpt_response})
 
             return Response({"answer": gpt_response}, status=status.HTTP_200_OK)
 
         except Exception as e:
-            print(f"Error in AskView: {e}")  # Отладка ошибок
+            print(f"Error in AskView: {e}")  
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
