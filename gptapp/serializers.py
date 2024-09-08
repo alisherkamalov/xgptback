83f923bd0adac4c09a6cf17c48b0f9ddaa5af680
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-08 15:51:12 +0500

    Update serializers.py

--- gptapp/serializers.py
+++ gptapp/serializers.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
 class QuestionSerializer(serializers.Serializer):
-    question = serializers.CharField(max_length=500)
+    question = serializers.CharField()
