7f123888c5823b393d3aa2e134340a4ce0b6a186
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2025-01-19 21:56:58 +0500

    Update views.py

--- gptapp/views.py
+++ gptapp/views.py
@@ -20,7 +20,7 @@ class AskView(APIView):
 
             response = g4f.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                provider=g4f.Provider.FastGpt,
+                provider=g4f.Provider.OpenaiChat,
                 messages=self.context
             )
             
